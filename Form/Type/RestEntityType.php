b0f8e8b0c0f10d5c749cde481f1f448ed2af0076
root <root@localhost.localdomain>
2016-09-04 06:38:07 +0100

    Initial commit

---
+++ Form/Type/RestEntityType.php
@@ -0,0 +1,72 @@
+<?php
+
+
+namespace ehassanov\RestGeneratorBundle\Form\Type;
+
+use Doctrine\ORM\EntityManager;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use ehassanov\RestGeneratorBundle\Form\DataTransformer\ArrayToIdTransformer;
+
+/**
+ * Class VoryxEntityType
+ * @package Voryx\RESTGeneratorBundle\Form\Type
+ */
+class RestEntityType extends EntityType
+{
+
+    /**
+     * @var EntityManager
+     */
+    private $em;
+
+    /**
+     * @param EntityManager $em
+     */
+
+    public function __construct(EntityManager $em)
+    {
+        $this->em = $em;
+    }
+
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $view_transformer = new ArrayToIdTransformer($this->em, null);
+        $builder->addViewTransformer($view_transformer);
+//        $model_transformer = new UserToUsernameTransformer()
+    }
+
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(
+            array(
+                'invalid_message' => 'The selected entity does not exist',
+                'csrf_protection' => false,
+            )
+        );
+    }
+
+    /**
+     * @return string
+     */
+    public function getParent()
+    {
+        return 'entity';
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'rest_entity';
+    }
+}
